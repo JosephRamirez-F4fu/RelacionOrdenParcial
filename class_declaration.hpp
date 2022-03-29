96dfadc0360a6a85bdd19768a293c03f93ddcab3
Joseph Ramirez Sarmiento <86686756+JosephRamirez-F4fu@users.noreply.github.com>
2022-03-29 19:30:28 +0000

    Pre-Realizacion de las clases y fuciones, ademas de las bibliotecas a usar.

--- class_declaration.hpp
+++ class_declaration.hpp
@@ -5,3 +5,34 @@
 #include <string>
 
 using namespace std;
+
+Class numero {
+	public:
+		numero(int _n);
+		~numero();
+		void Divisores_numero();
+		void ingresar_sub_conjunto_Div(int _n);
+	private:
+			int *n;
+			vector<int> Divisores;
+			vector<int> sub_conjunt_Div;
+};
+class Conjunto_Ordenado {
+	public:
+			Par_Ordenado(numero *_objnumero);
+			~Par_Ordenado();
+			void Imprimir_Relacion_Subconjunto_Divisores();
+	private:
+			numero *obj_numero
+			
+};
+class Diagrama_Hasse {
+			public:
+				Diagrama_Hasse();
+				~Diagrama_Hasse();
+			void Dibujar_Diagrama_Hase();
+			private:
+				Conjunto_Ordenado *obj_CO;
+};
+
+
